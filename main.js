ee20e70c76f473bd8a892d8a8751eff85b2a3084
Serhii <adamenko0serhii@gmail.com>
2022-11-08 12:27:59 +0200

    error in JS

--- main.js
+++ main.js
@@ -1,32 +1,96 @@
 const questions = [
-	{
-		question: "Какой язык работает в браузере?",
-		answers: ["Java", "C", "Python", "JavaScript"],
-		correct: 4,
-	},
-	{
-		question: "Что означает CSS?",
-		answers: [
-			"Central Style Sheets",
-			"Cascading Style Sheets",
-			"Cascading Simple Sheets",
-			"Cars SUVs Sailboats",
-		],
-		correct: 2,
-	},
-	{
-		question: "Что означает HTML?",
-		answers: [
-			"Hypertext Markup Language",
-			"Hypertext Markdown Language",
-			"Hyperloop Machine Language",
-			"Helicopters Terminals Motorboats Lamborginis",
-		],
-		correct: 1,
-	},
-	{
-		question: "В каком году был создан JavaScript?",
-		answers: ["1996", "1995", "1994", "все ответы неверные"],
-		correct: 2,
-	},
+  {
+    question: "Яка мова працює у браузері?",
+    answers: ["Java", "C", "Python", "JavaScript"],
+    correct: 4,
+  },
+  {
+    question: "Що означає CSS?",
+    answers: [
+      "Central Style Sheets",
+      "Cascading Style Sheets",
+      "Cascading Simple Sheets",
+      "Cars SUVs Sailboats",
+    ],
+    correct: 2,
+  },
+  {
+    question: "Що означає HTML?",
+    answers: [
+      "Hypertext Markup Language",
+      "Hypertext Markdown Language",
+      "Hyperloop Machine Language",
+      "Helicopters Terminals Motorboats Lamborginis",
+    ],
+    correct: 1,
+  },
+  {
+    question: "У якому році було створено JavaScript?",
+    answers: ["1996", "1995", "1994", "усі відповіді неправильні"],
+    correct: 2,
+  },
 ];
+
+//Finding elements
+const headerContainer = document.querySelector("#header");
+const listContainer = document.querySelector("#list");
+const submitBtn = document.querySelector("#submit");
+
+//Game variables
+let score = 0; //
+let questionIndex = 0; //
+
+clearPage();
+showQuestion();
+submitBtn.onclick = checkAnswer;
+
+function clearPage() {
+  headerContainer.innerHTML = "";
+  listContainer.innerHTML = "";
+}
+
+function showQuestion() {
+  // Вопрос
+
+  const headerTemplate = `<h2 class="title">%title%</h2>`;
+  const title = headerTemplate.replace(
+    "%title%",
+    questions[questionIndex]["question"]
+  );
+  headerContainer.innerHTML = title;
+
+  // Варианты ответов
+  let answerNumber = 1;
+  for (answerText of questions[questionIndex]["answers"]) {
+    const questionTemplate = `<li><label><input value='%number%' type="radio" class="answer" name="answer" /><span>%answer%</span></label></li>`;
+
+    const answerHTML = questionTemplate
+      .replace("%number%", answerText)
+      .replace("%answer%", answerText);
+
+    listContainer.innerHTML = listContainer.innerHTML + answerHTML;
+    answerNumber++;
+  }
+}
+function checkAnswer() {
+  // console.log("checkAnswer started!");
+  // находим выбраную радио кнопку
+  const checkedRadio = listContainer.querySelector(
+    'input[type="radio"]:checked'
+  );
+  console.log(checkedRadio);
+  //если ответ не выбран ничего не делаем выходим из функции
+  if (!checkedRadio) {
+    submitBtn.blur();
+    return;
+  }
+  console.log(checkedRadio.value);
+
+  const userAnswer = parseInt(checkedRadio.value);
+
+  //если ответ верно - увеличиваем счет
+  if (userAnswer === questions[questionIndex]["correct"]) {
+    score++;
+    console.log("score", score);
+  }
+}
